cb3b6d545e978539ae758080ef451f53e86958cc
christian fish <cfishmusic@hotmail.com>
2022-04-07 23:22:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = [
+    'Milo', 
+    'Otis', 
+    'Garfield'
+];
+
+const destructivelyAppendCat = () => {
+    return cats.push('Ralph');
+}
+
+const destructivelyPrependCat = () => {
+    return cats.unshift('Bob');
+}
+
+const destructivelyRemoveLastCat = () => {
+    return cats.pop('Garfield');
+}
+
+const destructivelyRemoveFirstCat = () => {
+    return cats.shift();
+}
+
+const appendCat = () => {
+    return [...cats, 'Broom'];
+}
+
+const prependCat = () => {
+    return ['Arnold', ...cats];
+}
+
+const removeLastCat = () => {
+    return cats.slice(0, 2);
+}
+
+const removeFirstCat = () => {
+    return cats.slice(-2);
+}
+//why did cats.slice(1, 2) not do the same thing
+
+
+
